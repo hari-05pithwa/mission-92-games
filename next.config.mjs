994d262ae5a58273b1e67142b770381e240e9b4a
Hari Pithwa <haripithwa2005@gmail.com>
2026-02-10 10:36:39 +0530

    changes on 10 feb

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    turbopack: {
+      root: '.',
+    },
+  },
 };
 
 export default nextConfig;
